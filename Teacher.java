5c3ba54e0e84a7008a61aa8136bd5779c3b39127
Dmitry <falconhidegames@gmail.com>
2024-03-29 19:00:05 +0300

    Made Teacher

---
+++ Teacher.java
@@ -0,0 +1,11 @@
+package HW_5;
+
+public class Teacher {
+    public int id;
+    public String name;
+
+    public Teacher(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
